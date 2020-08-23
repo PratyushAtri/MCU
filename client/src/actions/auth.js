7363c97aa6df7fe0a0a2c7bcd5c52f7c30ea1a57
PratyushAtri <63645072+PratyushAtri@users.noreply.github.com>
2020-08-23 19:42:03 +0530

    NavLinks Auth user

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,8 @@ import {
     USER_LOADED,
     AUTH_ERROR,
     LOGIN_FAIL,
-    LOGIN_SUCCESS
+    LOGIN_SUCCESS,
+    LOGOUT
 } from "./types";
 import setAuthToken from "../utils/setauthToken";
 
@@ -94,4 +95,9 @@ export const login = ( email, password ) => async dispatch => {
             type: LOGIN_FAIL
         });
     }
+};
+
+// Logout / Clear Profile
+export const logout = () => dispatch => {
+    dispatch({ type: LOGOUT });
 };
