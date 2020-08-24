522b27dc6f56c66dd691052b3cbfe33d454db29a
PratyushAtri <63645072+PratyushAtri@users.noreply.github.com>
2020-08-24 12:39:19 +0530

    Edit Profile FrontEnd

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/profile-forms/CreateProfile";
+import EditProfile from "./components/profile-forms/Edit-profile";
 
 // CSS
 import './App.css';
@@ -44,6 +45,7 @@ const App = () => {
                           <Route exact path='/login' component={ Login } />
                           <PrivateRoute exact path='/dashboard' component={ Dashboard } />
                           <PrivateRoute exact path='/create-profile' component={ CreateProfile } />
+                          <PrivateRoute exact path='/edit-profile' component={ EditProfile } />
                       </Switch>
                   </section>
               </Fragment>
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from "../../actions/profile";
+import DashboardActions from "./DashboardActions";
 import Spinner from "../layout/Spinner";
 
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
@@ -21,7 +22,9 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
             </p>
             {
                 profile !== null ?
-                    <Fragment> Has a profile </Fragment>
+                    <Fragment>
+                        <DashboardActions />
+                    </Fragment>
                     :
                     <Fragment>
                         <p>
